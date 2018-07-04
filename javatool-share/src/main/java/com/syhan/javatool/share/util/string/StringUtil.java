a833621db713dfe92027826e95ec2a090f261c2a
Han Seongyeong <syhan@nextree.co.kr>
2018-07-04 18:04:50 +0900

    fix recommended var name

--- javatool-share/src/main/java/com/syhan/javatool/share/util/string/StringUtil.java
+++ javatool-share/src/main/java/com/syhan/javatool/share/util/string/StringUtil.java
@@ -4,7 +4,26 @@ public class StringUtil extends org.apache.commons.lang3.StringUtils {
     //
     public static String getRecommendedVariableName(String name) {
         //
+        if (isJavaKeyword(name)) {
+            return name + "00";
+        }
         return toFirstLowerCase(name);
+    }
+
+    private static final String[] JAVA_KEYWORDS = {"abstract","boolean","break","byte","catch","char","class","continue",
+        "default","do","double","else","extends","finally","float","for","if","implements","import","instanceof",
+        "int","interface","long","native","new","null","package","private","protected","public","return","short",
+        "static","super","switch","sychronized","this","throw","throws","try","void","while"};
+
+    private static boolean isJavaKeyword(String name) {
+        //
+        String varName = name.toLowerCase();
+        for (String keyword : JAVA_KEYWORDS) {
+            if (keyword.equals(varName)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public static String toFirstLowerCase(String str) {
